eab903338d2cc2bab9887bafb51b1a91ffe854b5
ninad <ninad2eyes@gmail.com>
2024-11-29 20:19:31 -0500

    added header block

--- platformer/main.cpp
+++ platformer/main.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: Ninad Moharir
+* Assignment: Platformer
+* Date due: 2023-11-23, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #define GL_SILENCE_DEPRECATION
 #define GL_GLEXT_PROTOTYPES 1
 #define FIXED_TIMESTEP 0.0166666f
